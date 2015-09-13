95cccd6cb827f676f99a63f112a8d9d84c94af70
Michael Crockett <michael.crockett@gmail.com>
2015-09-13 13:32:01 -0700

    added rgb led control

---
+++ arduino-rgb-led/README.md
@@ -0,0 +1,3 @@
+Wiring Info and Sketch:
+
+http://wiring.org.co/learning/basics/rgbled.html
---
+++ arduino-rgb-led/RGB-LED/RGB-LED.ino
@@ -0,0 +1,29 @@
+int REDPin = 4;    // RED pin of the LED to PWM pin 4
+int GREENPin = 5;  // GREEN pin of the LED to PWM pin 5
+int BLUEPin = 6;   // BLUE pin of the LED to PWM pin 6
+int brightness = 255; // LED brightness
+int increment = 25;  // brightness increment
+
+void setup()
+{
+  pinMode(REDPin, OUTPUT);
+  pinMode(GREENPin, OUTPUT);
+  pinMode(BLUEPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  brightness = brightness + increment;  // increment brightness for next loop iteration
+
+  if (brightness <= 0 || brightness >= 255)    // reverse the direction of the fading
+  {
+    increment = -increment;
+  }
+  brightness = constrain(brightness, 0, 255);
+  analogWrite(REDPin, brightness);
+  //analogWrite(GREENPin, brightness);
+  //analogWrite(BLUEPin, brightness);
+
+  delay(300);  // wait for 20 milliseconds to see the dimming effect
+}
